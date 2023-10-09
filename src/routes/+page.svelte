430ec41d1dad010c4d3fdcc343426d34ad02693b
Eivind Dalholt <eividalh@stud.ntnu.no>
2023-10-10 00:37:32 +0200

    add some styles ✨

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -27,6 +27,7 @@
 
     // Event listener for key press and capture letter
     const onKeyDown = (/** @type {{ key: string; }} */ event) => {
+      if (correct) return
       if (event.key === 'Enter') {
         if (word.length < 5) return
         handleGuess()
@@ -36,44 +37,79 @@
         word = word.slice(0, -1)
         return
       }
+      if (event.key.length > 1) {
+        return
+      }
       if (word.length >= 5) return
-      word += event.key
+      word += event.key.toLowerCase()
     }
   </script>
   
   <main>
-    <h1>Guess the word!</h1>
 
+    <h1 class="heading">Yet Another Wordle</h1>
 
-    {#each prevWords as word}
-    <div class="wordRow">
-    {#each word as letter}
-      <div class="letter" style="border-color: {letter.placement === 1 ? 'green' : letter.status === 1 ? 'yellow' : 'black'}">{letter.letter}</div>
-    {/each}
+    <div id="game">
+      {#each prevWords as word}
+      <div class="wordRow">
+      {#each word as letter}
+        <div class="letter" style="border-color: {letter.placement === 1 ? '#005c26' : letter.status === 1 ? '#b59f3b' : '#689cc5'}">{letter.letter.toUpperCase()}</div>
+      {/each}
+      </div>
+      {/each}
+
+      {#if !correct}
+      <div class="wordRow">
+      {#each word.split('') as letter}
+        <div class="letter">{letter.toUpperCase()}</div>
+      {/each}
+      {#if (word.length < 5)}
+      {#each Array.from({ length: 5 - word.length }) as _}
+        <div class="letter"> </div>
+      {/each}
+      {/if}
+      </div>
+      {/if}
+      {#if correct}
+      <h1 class="heading">Correct word!</h1>
+      {/if}
     </div>
-    {/each}
 
-    {#if !correct}
-    <div class="wordRow">
-    {#each word.split('') as letter}
-      <div class="letter">{letter}</div>
-    {/each}
-    {#if (word.length < 5)}
-    {#each Array.from({ length: 5 - word.length }) as _}
-      <div class="letter"> </div>
-    {/each}
-    {/if}
-    </div>
-    {/if}
-    {#if correct}
-    <h1>Correct word!</h1>
-    {/if}
   </main>
   
   <style>
+
+    * {
+      margin: 0;
+      padding: 0;
+    }
+    .heading {
+      color: white;
+      font-family: 'Times New Roman', Times, serif;
+    }
+    :global(body){
+      background-color: #19242f;
+      background-image: url('waveBg.svg');
+      background-repeat: no-repeat;
+      background-size: cover;
+      min-height: 100vh;
+      background-position: bottom;
+    }
     main {
       text-align: center;
-      padding: 2rem;
+      margin-top: 30vh;
+      margin-right: 40vw;
+    }
+
+    @media only screen and (max-width: 600px) {
+      main {
+        margin-right: 0;
+      }
+    }
+
+    #game {
+      margin-top: 5vh;
+      margin-bottom: 60vh;
     }
     .wordRow {
       display: flex;
@@ -82,7 +118,9 @@
       margin: 2rem;
     }
     .letter {
-      border: 4px solid black;
+      border: 4px solid #689cc5;
+      font-family: Verdana, Geneva, Tahoma, sans-serif;
+      color: white;
       width: 2rem;
       height: 2rem;
       margin: 0 0.5rem;
